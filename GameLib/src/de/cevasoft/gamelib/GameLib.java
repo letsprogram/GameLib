a48c131f05af8f713d4a6b8311726b917db9d94f
letsprogram <letsprogram@cevasoft.de>
2014-08-19 10:13:01 +0200

    Add FPS counter and temprary slow down

--- GameLib/src/de/cevasoft/gamelib/GameLib.java
+++ GameLib/src/de/cevasoft/gamelib/GameLib.java
@@ -16,10 +16,29 @@ public class GameLib {
 		Window window = new Window(320, 200);
 		window.add(sceneView);
 		window.setVisible(true);
-		int tick = 0;
+		int tick = 0, frames = 0;
+		long lastTime = System.currentTimeMillis();
+		
+		// Game loop
 		while(running)
 		{
+			// FPS Counter			
 			tick++;
+			frames++;
+			long now = System.currentTimeMillis();
+			if(now - lastTime >= 1000){
+				System.out.println(frames);
+				lastTime = now;
+				frames = 0;
+			}
+			
+			// TODO: Temporary FPS controller needs to be automated
+			try {
+				Thread.sleep(12);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
 			for(int i = 0; i < 320 * 200; i++){
 				sceneView.pixels[i] = i + tick;
 			}
